7f22f7c553a0992c482f081b0efa544f5d17b7c4
cpicha20 <cpichardowps@gmail.com>
2023-11-08 09:26:50 -0500

    initial commit

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,19 +1,21 @@
 const router = require('express').Router();
-const { User } = require('../models');
+const { User, Blogpost } = require('../models');
 const withAuth = require('../utils/auth');
 
-// Prevent non logged in users from viewing the homepage
-router.get('/', withAuth, async (req, res) => {
-  try {
-    const userData = await User.findAll({
-      attributes: { exclude: ['password'] },
-      order: [['name', 'ASC']],
-    });
 
-    const users = userData.map((project) => project.get({ plain: true }));
+router.get('/', async (req, res) => {
+  try {
+    const blogData = await Blogpost.findAll({ 
+    include : [User]
+    });
+    
+
+    const blogposts = blogData.map((project) => project.get({ plain: true }));
+
+    console.log(blogData);
 
     res.render('homepage', {
-      users,
+      blogposts,
       // Pass the logged in flag to the template
       logged_in: req.session.logged_in,
     });
--- models/index.js
+++ models/index.js
@@ -1,3 +1,21 @@
 const User = require('./User');
+const Blogpost = require('./Blogpost');
+const Comments = require('./Comments');
 
-module.exports = { User };
+
+
+User.hasMany(Blogpost,{
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE',
+});
+
+Blogpost.belongsTo(User, {
+    foreignKey: 'user_id',
+  });
+
+
+//   Blogpost.hasMany(Comments, {
+//     foreignKey: 'blogpost_id',
+//   });
+  
+  module.exports = { User , Blogpost , Comments };
